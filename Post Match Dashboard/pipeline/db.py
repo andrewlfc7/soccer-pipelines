3a655e6d0d5a904000bfb232a56e335ab286460b
Andrew <105604530+andrewlfc7@users.noreply.github.com>
2023-11-28 08:56:55 -0500

    Update db.py

--- Post Match Dashboard/pipeline/db.py
+++ Post Match Dashboard/pipeline/db.py
@@ -24,7 +24,7 @@ data['competition'] = competition
 from sqlalchemy import create_engine
 
 
-engine = create_engine('postgresql://postgres:Liverpool19@72.74.59.108:5432/soccer')
+engine = create_engine('')
 
 
 
